f2cbb50f0ea8bf47ff160a043eb22ffbcd2ae3e0
rowjay007 <rowlives@gmail.com>
2022-07-20 12:10:34 +0100

    map cart component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import ProductDetails from "./components/ProductDetails";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [savedCount, setSavedCount] = useState(0);
   return (
-    <div className="">
-      <Navbar />
-      <ProductDetails />
-      <h1>Hello, World!</h1>
+    <div className="relative min-h-screen bg-white lg:grid lg:grid-rows-[1fr_auto]">
+      <div className="mx-auto max-w-6xl">
+        <Navbar 
+// @ts-ignore
+        savedCount={savedCount} setSavedCount={setSavedCount} />
+
+        <main className="lg:mt-20 lg:flex lg:items-center lg:justify-center lg:gap-24 lg:px-12">
+
+          <ProductDetails
+            // @ts-ignore
+            count={counter}
+            setCounter={setCounter}
+            setSavedCount={setSavedCount}
+          />
+        </main>
+      </div>
+
+     
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const sneaker = [
+  {
+    id: "0",
+    brand: "Sneaker Company",
+    productName: "Fall Limited Edition Sneakers",
+    description: `These low-profile sneakers are your perfect casual wear companion.
+  Featuring a durable rubber outer sole, they'll whithstand everything the
+  weather can offer.`,
+    initialPrice: 250,
+    discount: 50,
+    discountPrice: 125,
+  },
+];
